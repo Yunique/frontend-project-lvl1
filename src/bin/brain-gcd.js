b75b231864f6157a591b8ec6c232615920816895
Yunik Stepanyan <yurastep92@gmail.com>
2019-10-11 23:18:56 +0400

    linter

--- src/bin/brain-gcd.js
+++ src/bin/brain-gcd.js
@@ -3,4 +3,3 @@
 import game from '../games/logicGcd';
 
 game();
-
--- src/games/logicGcd.js
+++ src/games/logicGcd.js
@@ -3,7 +3,7 @@ import { greeting, rand, gameProcess } from '../engine';
 const getExp = () => {
   const minRange = 0;
   const maxRange = 100;
-  
+
   const firstNum = rand(minRange, maxRange);
   const secondNum = rand(minRange, maxRange);
   const exp = `${firstNum} ${secondNum}`;
@@ -16,19 +16,19 @@ const calcExp = (expression) => {
   for (let index = 0; expression[index] !== ' '; index += 1) {
     numFirst += expression[index];
   }
-  
+
   for (let index = 0; index <= expression.length; index += 1) {
     if (expression[index] === ' ') {
-    numSecond = expression.slice(index + 1);
+      numSecond = expression.slice(index + 1);
     }
   }
   numFirst = Number(numFirst);
   numSecond = Number(numSecond);
-  
+
   const gcd = (x, y) => {
     if (y === 0) return Math.abs(x);
     return gcd(y, (x % y));
-  }
+  };
   return gcd(numFirst, numSecond).toString();
 };
 
@@ -38,4 +38,3 @@ const game = () => {
 };
 
 export default game;
-
