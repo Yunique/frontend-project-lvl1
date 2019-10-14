49b3968bf41f308b4ebcba877fdd83990d3f5815
Yunik Stepanyan <yurastep92@gmail.com>
2019-10-14 23:56:21 +0400

    fix brain-prime

--- src/games/logicPrime.js
+++ src/games/logicPrime.js
@@ -13,7 +13,7 @@ const getRightAnswer = (number) => {
     for (let counter = 2; counter <= (number / 2); counter += 1) {
       if (number % counter === 0) return 'no';
     } return 'yes';
-  }; return isPrime;
+  }; return isPrime();
 };
 
 const game = () => {
