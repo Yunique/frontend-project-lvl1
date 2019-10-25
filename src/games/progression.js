f8813c4a70acbdcbb733a1a680eb1c93ec13d984
Yunik Stepanyan <yurastep92@gmail.com>
2019-10-25 14:29:50 +0300

    refactor progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -10,28 +10,33 @@ const maxStep = 10;
 
 const progressionLength = 10;
 
-const getProgression = (firstElement, progressionStep, indexOfMissingElement) => {
+const getProgression = (firstElement, progressionStep, progressionLength) => {
   const progression = [];
-
   for (let index = 0; index < progressionLength; index += 1) {
-    if (index === indexOfMissingElement) progression.push('..');
-    else progression.push(firstElement + (progressionStep * index));
+    progression.push(firstElement + (progressionStep * index));
   }
   return progression;
 };
 
-const getMissingElement = (progression) => {
-  const indexOfMissingElement = progression.indexOf('..');
-  return (progression[indexOfMissingElement + 1] + progression[indexOfMissingElement - 1]) / 2;
+const getQuestion = (progession, indexOfMissingElement) => {
+  const question = progession.slice();
+  question[indexOfMissingElement] = '..';
+  return question.join(' ');
+};
+
+const getRightAnswer = (firstElement, progressionStep, indexOfMissingElement) => {
+  const missingElement = firstElement + (progressionStep * indexOfMissingElement);
+  return missingElement.toString();
 };
 
 const getQuestionAndRightAnswer = () => {
   const firstElement = rand(min, max);
   const progressionStep = rand(min + 1, maxStep);
   const indexOfMissingElement = rand(min + 1, progressionLength - 1);
-  const progression = getProgression(firstElement, progressionStep, indexOfMissingElement);
-  const question = progression.join(' ');
-  const rightAnswer = getMissingElement(progression).toString();
+
+  const progression = getProgression(firstElement, progressionStep, progressionLength);
+  const question = getQuestion(progression, indexOfMissingElement);
+  const rightAnswer = getRightAnswer(firstElement, progressionStep, indexOfMissingElement);
 
   return cons(question, rightAnswer);
 };
